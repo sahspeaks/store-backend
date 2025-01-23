dea38e857a0f9870f139db648ee29708196ae9ab
Sah Abhishek Kumar <akroy2456@gmail.com>
2025-01-23 19:57:47 +0530

    chnged url

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const start = async () => {
   const app = express();
   // CORS configuration
   const corsOptions = {
-    origin: "https://moonsflare.netlify.app", // Your frontend URL ""  http://localhost:5173  https://moonsflare.netlify.app/
+    origin: "https://moonsflare.com", // Your frontend URL "http://localhost:5173"    https://moonsflare.netlify.app/
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
